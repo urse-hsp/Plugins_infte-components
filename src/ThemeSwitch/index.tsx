a83b213fe2444ae89b10e309ed7a710ed5c33373
p <hsp_email@163.com>
2023-09-04 16:52:28 +0800

    style: back

--- src/ThemeSwitch/index.tsx
+++ src/ThemeSwitch/index.tsx
@@ -1,8 +1,11 @@
+import { theme } from 'antd';
 import classNames from 'classnames';
 import React from 'react';
 import dark_img from './images/dark.png';
 import light_img from './images/light.png';
 import style from './index.module.scss';
+
+const { useToken } = theme;
 
 type ThemeSwitchProps = {
   darkMode?: boolean;
@@ -10,11 +13,13 @@ type ThemeSwitchProps = {
 };
 const ThemeSwitch = (props: ThemeSwitchProps) => {
   const { darkMode = false, setTheme } = props;
+  const { token } = useToken();
 
   return (
     <div
       className={classNames(style.themeSwitch, 'cursor')}
       onClick={() => setTheme?.()}
+      style={{ backgroundColor: token.colorBorder }}
     >
       <img src={light_img} alt="" />
       <img src={dark_img} alt="" />
